7282a272e0cd1a8701059e69b3f5c81bda8bc05e
Ravensbergen Timo <ravenst1@sdcc-login02.iter.org>
2023-03-31 09:50:28 +0200

    initial commit with dir structure

---
+++ tools/logsout2struct.m
@@ -0,0 +1,80 @@
+function [out,tsout] = logsout2struct(logsout)
+% function [out,tsout] = logsout2struct(logsout)
+% out: structure with data
+% tsout: structure with timeseries
+%
+% Convert logged data to structure
+
+%%
+assert(isa(logsout,'Simulink.SimulationData.Dataset'),'logsout is of wrong type');
+
+% determine smallest common time base
+for iel = 1:logsout.numElements
+   % seek longest common base to use for all timeseries
+    ts = logsout.getElement(iel).Values;
+    if ~isa(ts,'timeseries')
+        warning('signal %s is not a timeseries, please log only buses',logsout.getElementNames{iel})
+        continue
+    end
+    base_timevector = ts.Time;
+    if iel == 1
+      % do nothing, keep base time vector
+    elseif  ts.TimeInfo.Length>0 && ts.TimeInfo.Length < numel(base_timevector)
+      base_timevector = ts.Time;
+    end
+end 
+%base_ts = timeseries(zeros(size(base_timevector)),base_timevector);
+
+for iel = 1:logsout.numElements
+    myElement = logsout.getElement(iel);
+    if ~isa(myElement.Values,'timeseries')
+        continue
+    end
+    
+    if myElement.Values.TimeInfo.Length>0
+    ts = resample(myElement.Values,base_timevector); % resample to common base
+    name = myElement.Name;
+    else
+        warning('Signal %s has empty time base, skipping',name)
+        continue;
+    end
+    
+    % skip signal if name is empty or if not properly defined
+    if isempty(name) 
+        warning('Signal skipped, because signal name empty or not defined in Simulink')
+        continue; 
+    end
+    if any(name=='<')
+%         warning('Signal skipped, because signal name contains <>, Simulink took it probably from previous block')
+        name = strrep(strrep(name, '<' , ''), '>' , '');
+%         continue
+    end
+        
+    try
+    tsdata.(name) = ts;
+        % store as structure
+        % make sure time index always last
+        if isstruct(ts)
+            ts = ts.value; % if it's a DCS bus signal
+        end
+        if ts.IsTimeFirst % case [nt x m]
+            dd=ts.Data';
+        elseif numel(ts.Data)==length(ts.Data) % case [1 x 1 x nt]
+            dd=squeeze(ts.Data)';
+        else % case [a x b x nt];
+            dd=squeeze(ts.Data);
+        end
+        datastruct.(name) = dd;
+    catch me
+    end
+    
+    if ~isfield(datastruct,'time')
+        datastruct.time = ts.Time;
+    end
+end
+
+% out
+out = datastruct;
+tsout = tsdata;
+
+return
---
+++ tools/prune_ids_structure.m
@@ -0,0 +1,64 @@
+function var_out = prune_ids_structure(var_in)
+%
+% ret = prune_ids_structure(structure_in)
+% 
+% Removes empty, and empty representing, values and structures from IDS 
+% structure.
+%
+    empty_values = {-9.0000e+40, int32(-999999999), -999999999};
+
+%     disp('-----')
+%     var_in
+    if isa(var_in, 'struct')
+        %
+        % structure
+        %
+        fn = fields(var_in);
+        for f_idx = 1 : length(fn)
+%             disp('var_in.(fn{f_idx}):')
+%             var_in.(fn{f_idx})
+            
+            dummy = prune_ids_structure(var_in.(fn{f_idx}));
+            if (isempty(dummy))
+                var_in = rmfield(var_in, fn{f_idx});
+            else
+                var_in.(fn{f_idx}) = dummy;
+            end
+        end % end for structure fields    
+        
+        fn = fields(var_in);
+        tf = cellfun(@(c) isempty(var_in.(c)), fn);
+        var_out = rmfield(var_in, fn(tf));
+        if isempty(fieldnames(var_out))
+            var_out = '';
+        end
+    elseif isa(var_in, 'char')
+        %
+        % char/string
+        %
+        var_out = var_in;
+    elseif isa(var_in, 'int32') | isa(var_in, 'double')
+        %
+        % integer or double
+        % 
+        var_out = var_in;
+        for e_idx = 1:length(empty_values)
+            if all(var_out == empty_values{e_idx})
+                var_out = '';
+            end
+        end
+    elseif isa(var_in, 'cell')
+        l = length(var_in);
+        var_out = var_in;
+        for c_idx=1:l
+            dummy = prune_ids_structure(var_in{c_idx});
+            if ~isempty(dummy)
+                var_out{c_idx} = dummy;
+            end
+        end
+    else
+        error('NOT COVERED YET')
+    end
+
+    
+end
