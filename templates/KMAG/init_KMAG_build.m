628d0e2eb031d6d1643b184b60ee4bfc1599b6be
Ravensbergen Timo <ravenst1@4504ds-srv-0024.codac.iter.org>
2023-05-16 11:52:15 +0000

    logged reference waveforms for KMAG

--- templates/KMAG/init_KMAG_build.m
+++ templates/KMAG/init_KMAG_build.m
@@ -5,10 +5,54 @@ clear; clc; bdclose all;
 % force close all zombie sldd
 Simulink.data.dictionary.closeAll('-discard')
 
-obj_KMAG = pcssp_KMAG_module_obj();
+writedata = 0; % flag to export logged data to txt
 
+%% init slx model
+obj_KMAG = pcssp_KMAG_module_obj();
 obj_KMAG.init;
 obj_KMAG.setup;
 
 %% build
 obj_KMAG.build;
+
+%% prep logged data for RTF export
+if writedata
+    load('KMAG_logged.mat');
+    KMAGin = logsout2struct(KMAG_logged);
+    fieldN = fieldnames(KMAGin);
+
+    for ii = 1:length(fieldN)
+        writematrix(transpose(KMAGin.(fieldN{ii})),['KMAG_in_' fieldN{ii}],'FileType','text');
+
+    end
+end
+
+%% write XML parameter structure
+xml_out.NameAttribute = "KMAG";
+xml_out.TypeAttribute = "SimulinkBlock";
+
+
+
+names = ["LibraryPath","BlockName","P","I","D","N","Ke"];
+Paramstrings = ["~/pcssp_KMAG/build/libpcssp.so","KMAG"];
+
+
+for ii = 1:length(names)
+    xml_out.Parameter(ii).NameAttribute =  names(ii);
+
+    if ii>2
+    xml_out.Parameter(ii).ValueAttribute = jsonencode(eval(sprintf('pcssp_KMAG_%s.Value',names(ii))));
+    
+    else
+        xml_out.Parameter(ii).ValueAttribute = Paramstrings(ii);
+    end
+end
+
+
+%%
+
+% if writedata
+    writestruct(xml_out,'KMAG_params.xml',"StructNodeName","FunctionBlock");
+% end
+
+
