cbfc28dc551f52b2e660cdef944dc41998dc4aeb
Timo Ravensbergen <timoravensbergen@Timos-MacBook-Pro-2.local>
2024-08-02 09:48:08 +0200

    PID test bugfix: bus def outdated

--- templates/PID_example/modules/PID/pcssp_PID_test.m
+++ templates/PID_example/modules/PID/pcssp_PID_test.m
@@ -3,7 +3,7 @@ classdef pcssp_PID_test < pcssp_module_test
     % see pcssp_module test class for all standard pcssp module tests
     
     properties
-        algoobj = @(n_input)pcssp_PID_obj(3);
+        algoobj = @(n_input)pcssp_PID_obj(1);
         isCodegen = 1; % flag to trigger module tests for codeGen
     end  
     
